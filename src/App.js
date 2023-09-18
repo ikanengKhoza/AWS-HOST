09623192bff67e5914b26afb453bb2ab761ce501
Ika <ikaneng.khoza@gmail.com>
2023-09-18 18:40:04 +0100

    slogan added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <h1>
           Welcome To Embrace Tech
+          <p>Join me in discovering the exciting world of tech, coding and gadgets</p>
         </h1>
-      
       </header>
     </div>
   );
